dacdc20c42b454181e485e0af7938573a5bcb3f9
Aditya Ega Pratama <pratamaega513@gmail.com>
2025-06-07 04:48:09 +0700

    fix error dashboard

--- routes/web.php
+++ routes/web.php
@@ -26,16 +26,12 @@ Route::view('/teams', 'teams')->name('teams');
 // Authenticated & Verified Routes
 // -----------------------------
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-
     Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
 
     // Profile
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 // -----------------------------
@@ -84,6 +80,7 @@ Route::middleware(['auth', 'checkRole:admin'])->prefix('admin')->group(function 
 // -----------------------------
 Route::middleware(['auth', 'checkRole:guru'])->group(function () {
     // Tambahkan route khusus guru di sini
+   
 });
 
 // -----------------------------
