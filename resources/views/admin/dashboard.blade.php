5c7ac5c7f25976e6a732b7f422f2b679a42fb916
Adib Muhammad Zain <48612003+dibsmz@users.noreply.github.com>
2025-06-07 10:48:06 +0700

    perbaiki tambah guru

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,15 +7,17 @@
         <ol class="inline-flex items-center space-x-1 md:space-x-3">
             <li class="inline-flex items-center">
                     <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0h6"></path></svg>
-                    Pantau Statistik Data.
+                    Dashboard
                 </a>
             </li>
         </ol>
     </nav>
-        <div class="mb-6">
-            <h1 class="text-2xl font-bold text-gray-800 py-4">Selamat Datang di Dashboard</h1>
-        </div>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
+    
+    <div class="mb-6">
+        <h1 class="text-2xl font-bold text-gray-800 py-4">Selamat Datang di Dashboard</h1>
+    </div>
+    
+    <div class="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
         <div class="bg-blue-600 text-white rounded-lg p-6 shadow flex flex-col items-center">
             <div class="text-3xl font-bold">{{ $totalSekolah }}</div>
             <div class="mt-2">Data Sekolah</div>
